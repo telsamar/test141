dab77a675275e9f4fbe43cd7750e15730d1b71ae
telsamar <56135473+telsamar@users.noreply.github.com>
2023-07-12 12:29:06 +0300

    Update pyrogram_forever.py

--- pyrogram_forever.py
+++ pyrogram_forever.py
@@ -1,42 +1,57 @@
 import re
+import logging
+import sys
 from pyrogram import Client, filters
 from config import api_id, api_hash
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+message_count = 0
+
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
-with open('regex_patterns.txt') as f:
-    patterns = [re.compile(pattern.strip()) for pattern in f.readlines()]
+def read_patterns(file_name):
+    with open(file_name) as f:
+        return [re.compile(pattern.strip()) for pattern in f.readlines()]
 
-with open('channels.txt', 'r') as f:
-    channels = [line.strip().lower() for line in f]
+def read_channels(file_name):
+    with open(file_name, 'r') as f:
+        return [line.strip().lower() for line in f.readlines()]
+
+try:
+    patterns = read_patterns('regex_patterns.txt')
+    channels = read_channels('channels.txt')
+except Exception as e:
+    logger.error(f'Error reading files: {str(e)}')
+    sys.exit(1)
 
 @app.on_message(filters.channel)
 async def handle_channel_update(client, message):
-    nomer = 1
+    global message_count
     if message.chat.username in channels:
         if (message.text and any(pattern.search(message.text) for pattern in patterns)) or (message.caption and any(pattern.search(message.caption) for pattern in patterns)):
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             post_link = f"https://t.me/{message.chat.username}/{message.id}"
-            print(f'Nomer: {nomer}')
-            print(f"Новое сообщение в канале: {message.chat.title}")
-            print(f"Текст сообщения: {message.text}")
-            print(f'Caption: {message.caption}')
-            print(f'Link: {post_link}')
+            message_count += 1
+            logger.info(f'Nomer: {message_count}')
+            logger.info(f"Новое сообщение в канале: {message.chat.title}")
+            logger.info(f"Текст сообщения: {message.text}")
+            logger.info(f'Caption: {message.caption}')
+            logger.info(f'Link: {post_link}')
             if message.photo:
-                print("Есть фото")
+                logger.info("Есть фото")
             else:
-                print("Нет фото")
+                logger.info("Нет фото")
 
             if message.video:
-                print("Есть видео")
+                logger.info("Есть видео")
             else:
-                print("Нет видео")
+                logger.info("Нет видео")
             if message.document:
-                print("Есть прикрепленный документ")
+                logger.info("Есть прикрепленный документ")
             else:
-                print("Нет прикрепленного документа")
+                logger.info("Нет прикрепленного документа")
 
-            nomer += 1
-
-print("Программа запущена. Ожидание новых сообщений...")
+logger.info("Программа запущена. Ожидание новых сообщений...")
 app.run()
