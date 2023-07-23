925ae5e0e4a1e1f35573294aad8595ea075c945a
telsamar <56135473+telsamar@users.noreply.github.com>
2023-07-23 19:47:48 +0300

    read

--- Rpyrogram.py
+++ Rpyrogram.py
@@ -1,36 +1,44 @@
 import asyncio
-from pyrogram import Client, filters
-from datetime import datetime, timezone
-from docx import Document
-from docx.shared import Inches
-from docx.oxml import parse_xml
+import logging
 import os
 import re
-import logging
 import sys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
-from config import api_id, api_hash
+from datetime import datetime, timezone
 
+from config import api_id, api_hash
+from docx import Document
+from docx.oxml import parse_xml
+from docx.shared import Inches
+from pyrogram import Client
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 message_count = 0
+
+# Определение временного лимита из аргументов командной строки или установка значения по умолчанию
 if len(sys.argv) > 1:
     time_limit = int(sys.argv[1])
-# time_limit = 2
+else:
+    time_limit = 24
 
+# Функция для чтения регулярных выражений из файла
 def read_patterns(file_name):
     with open(file_name) as f:
         return [re.compile(pattern.strip()) for pattern in f.readlines()]
 
+# Функция для чтения списка каналов из файла
 def read_channels(file_name):
     with open(file_name, 'r') as f:
         return [line.strip().lower() for line in f.readlines()]
 
+# Попытка чтения регулярных выражений и списка каналов
 try:
     patterns = read_patterns('regex_patterns.txt')
     channels = read_channels('channels.txt')
@@ -39,49 +47,63 @@ except Exception as e:
     logger.error(f'Error reading files: {str(e)}')
     sys.exit(1)
 
+# Определение пути для сохранения документа и скриншотов
 filename = f'telegram_messages_{message_count}.docx'
 screenshot_path = "screen"
 
+# Настройка параметров веб-драйвера
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-
 webdriver_service = Service(ChromeDriverManager().install())
 
+# Попытка открытия существующего документа или создания нового
 try:
     doc = Document(filename)
     doc.clear()
-except:
+except Exception:
     doc = Document()
 
+# Основная  функция для работы с Telegram API и сохранения сообщений в документ Word
 async def main():
+    # Инициализация клиента Telegram с помощью Pyrogram
     app = Client("my_account2", api_id=api_id, api_hash=api_hash)
     await app.start()
     logger.info("Программа запущена...")
+
+    # Добавление временного лимита в Word документ
     doc.add_paragraph(f'time_limit: {time_limit}')
     doc.save(filename)
     global message_count
+
+    # Перебор списка каналов для обработки сообщений
     for channel_name in channels:
         async for message in app.get_chat_history(channel_name):
-            # print(message.chat.id)
+            # Вычисление разницы времени между текущим моментом и временем сообщения
             time_difference = datetime.now(timezone.utc) - message.date.astimezone(timezone.utc)
             time_difference_seconds = time_difference.total_seconds()
+            # Проверка соответствия разницы времени установленному лимиту
             if time_difference_seconds < time_limit * 3600:
-                post_link = f"https://t.me/{channel_name}/{message.id}"  
-    
-                if ((message.text is not None and message.text.strip() != "" and any(pattern.search(message.text) for pattern in patterns)) or 
-                    (message.caption is not None and message.caption.strip() != "" and any(pattern.search(message.caption) for pattern in patterns))):
+                post_link = f"https://t.me/{channel_name}/{message.id}"
 
+                # Проверка на наличие совпадений с регулярными выражениями в тексте или подписи сообщения
+                text_matches = message.text and any(pattern.search(message.text) for pattern in patterns)
+                caption_matches = message.caption and any(pattern.search(message.caption) for pattern in patterns)
+
+                if text_matches or caption_matches:
                     table = doc.add_table(rows=1, cols=2)
-                    for i, name in enumerate(["Номер", "Ссылка"]):
+                    column_names = ["Номер", "Ссылка"]
+
+                    for i, name in enumerate(column_names):
                         cell = table.cell(0, i)
                         cell.text = name
+
                     table.columns[0].width = Inches(1.0/3)
                     border_str = '<w:tcBorders xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"><w:top w:val="single"/><w:left w:val="single"/><w:bottom w:val="single"/><w:right w:val="single"/></w:tcBorders>'
+
                     for cell in table.rows[0].cells:
                         border_elm = parse_xml(border_str)
                         cell._element.get_or_add_tcPr().append(border_elm)
 
-                    # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                     message_count += 1
                     doc.add_paragraph(f'Nomer: {message_count}')
                     doc.add_paragraph(f'Text: {message.text}')
@@ -91,6 +113,7 @@ async def main():
                     cells = table.add_row().cells
                     cells[0].text = str(message_count)
                     cells[1].text = post_link
+
                     for cell in cells:
                         border_elm = parse_xml(border_str)
                         cell._element.get_or_add_tcPr().append(border_elm)
@@ -125,7 +148,6 @@ async def main():
                     driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
                     driver.get(post_link)
                     driver.set_window_size(1500, 3000)
-
                     element = driver.find_element(By.CLASS_NAME, "tgme_page_widget")
                     ActionChains(driver).move_to_element(element).perform()
 
@@ -137,12 +159,15 @@ async def main():
                     os.remove(screenshot_filename)
             else:
                 break
+
     print("Ожидание завершения - 3 секунды")
     await asyncio.sleep(3)
     await app.stop()
 
+
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
+
 try:
     loop.run_until_complete(main())
 finally:
--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
-import telebot
-from telebot import types
 import os
 import subprocess
 import time
+import telebot
+from telebot import types
 from config import api_bot, authorized_users
 
 bot = telebot.TeleBot(api_bot)
@@ -10,6 +10,7 @@ bot = telebot.TeleBot(api_bot)
 bot_is_busy = False
 waiting_users = []
 markup_cache = None
+
 
 def generate_markup(stage=None):
     keyboard = types.InlineKeyboardMarkup()
@@ -21,24 +22,29 @@ def generate_markup(stage=None):
         button_24 = types.InlineKeyboardButton(text="24", callback_data='24')
         keyboard.add(button_3, button_6, button_12, button_24)
     else:
-        button_run_script = types.InlineKeyboardButton(text="Запустить Rpyrogram.py", callback_data='run_script')
+        button_run_script = types.InlineKeyboardButton(
+            text="Запустить Rpyrogram.py", callback_data='run_script')
         keyboard.add(button_run_script)
 
     return keyboard
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     if message.chat.id not in authorized_users:
-        bot.send_message(message.chat.id, 'Извините, вы не авторизованы для использования этого бота.')
+        bot.send_message(
+            message.chat.id, 'Извините, вы не авторизованы для использования этого бота.')
         return
     markup = generate_markup()
     bot.send_message(message.chat.id, 'Пожалуйста, выберите:', reply_markup=markup)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     global bot_is_busy, waiting_users
     if call.message.chat.id not in authorized_users:
-        bot.answer_callback_query(call.id, 'Извините, вы не авторизованы для использования этого бота.')
+        bot.answer_callback_query(
+            call.id, 'Извините, вы не авторизованы для использования этого бота.')
         return
 
     if bot_is_busy:
@@ -48,11 +54,6 @@ def callback_query(call):
         return
 
     if call.data == 'run_script':
-        # if bot_is_busy:
-        #     bot.answer_callback_query(call.id, "Бот сейчас занят, попробуйте позже")
-        #     if call.message.chat.id not in waiting_users:
-        #         waiting_users.append(call.message.chat.id)
-        #     return
         markup = generate_markup(stage='run_script')
         bot.send_message(call.message.chat.id, 'Пожалуйста, выберите:', reply_markup=markup)
     elif call.data in ['3', '6', '12', '24']:
@@ -62,34 +63,44 @@ def callback_query(call):
             process = subprocess.Popen(['python3', 'Rpyrogram.py', call.data], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
             dots = "."
-            message = bot.send_message(call.message.chat.id, f"Скрипт начал работу{dots}")
+            message = bot.send_message(
+                call.message.chat.id, f"Скрипт начал работу{dots}")
             while process.poll() is None:
                 dots = "." if len(dots) > 2 else dots + "."
-                message = bot.edit_message_text(chat_id=call.message.chat.id, message_id=message.message_id, text=f"Скрипт начал работу{dots}")
+                message = bot.edit_message_text(chat_id=call.message.chat.id, 
+                                                message_id=message.message_id, 
+                                                text=f"Скрипт начал работу{dots}")
                 time.sleep(1)
 
             if process.returncode != 0:
-                bot.send_message(call.message.chat.id, "ОШИБКА! НЕОБХОДИМО ВМЕШАТЕЛЬСТВО СПЕЦИАЛИСТА. ОШИБКА! \nПроблема с парсингом")
+                bot.send_message(call.message.chat.id,
+                                 "ОШИБКА! НЕОБХОДИМО ВМЕШАТЕЛЬСТВО СПЕЦИАЛИСТА. ОШИБКА! \nПроблема с парсингом")
             else:
                 if os.path.isfile('telegram_messages_0.docx'):
                     with open('telegram_messages_0.docx', 'rb') as doc:
                         bot.send_document(call.message.chat.id, doc)
 
-                    bot.send_message(call.message.chat.id, "Скрипт успешно завершил работу, файл прикреплен")
+                    bot.send_message(call.message.chat.id,
+                                     "Скрипт успешно завершил работу, файл прикреплен")
                 else:
-                    bot.send_message(call.message.chat.id, "Скрипт завершил работу, но файл не найден")
+                    bot.send_message(
+                        call.message.chat.id, "Скрипт завершил работу, но файл не найден")
 
             markup = generate_markup()
-            bot.send_message(call.message.chat.id, "Бот снова свободен. Работаем дальше?", reply_markup=markup)
+            bot.send_message(
+                call.message.chat.id, "Бот снова свободен. Работаем дальше?", reply_markup=markup)
 
             for user in waiting_users:
-                bot.send_message(user, "Бот теперь свободен, вы можете начать новую задачу.", reply_markup=markup)
+                bot.send_message(
+                    user, "Бот теперь свободен, вы можете начать новую задачу.", reply_markup=markup)
             waiting_users = []
 
         except Exception as e:
-            bot.send_message(call.message.chat.id, "ОШИБКА! НЕОБХОДИМО ВМЕШАТЕЛЬСТВО СПЕЦИАЛИСТА. ОШИБКА! \n Проблема с ботом", reply_markup=generate_markup())
+            bot.send_message(call.message.chat.id,
+                             "ОШИБКА! НЕОБХОДИМО ВМЕШАТЕЛЬСТВО СПЕЦИАЛИСТА. ОШИБКА! \n Проблема с ботом", reply_markup=generate_markup())
             print(e)
         finally:
             bot_is_busy = False
+
 
 bot.infinity_polling()
